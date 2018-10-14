851b12233ecc032b95243586c3accbe0c3e6632f
AlmazSamatov <samatov.almaz@yandex.ru>
2018-10-14 15:33:26 +0300

    added new feature to balancer

--- src/balancer/balancer.py
+++ src/balancer/balancer.py
@@ -8,6 +8,18 @@ def balance(tree):
         if subtree.left is None and subtree.right is None and subtree.node_value is None:
             tree.node_value = None
 
+        branches = []
+
+        if subtree.right is not None:
+            branches.append(subtree.right)
+        if subtree.left is not None:
+            branches.append(subtree.left)
+        if subtree.node_value is not None:
+            branches.append(subtree.node_value)
+
+        if len(branches) == 1:
+            tree.node_value = branches[0]
+
         if tree.left is None and tree.right is None and subtree.left is None and subtree.right is None and \
                 isinstance(subtree.node_value, Tree) \
                 and ((isinstance(subtree.node_value.node_value, Tree) and
